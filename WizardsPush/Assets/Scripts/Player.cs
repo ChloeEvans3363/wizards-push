2f97ec2327557333d6f2f3ffe1ae186cd04699a9
Chloe Evans <cre3363@rit.edu>
2022-10-13 15:23:54 -0400

    Started developing the Push Tailsman

--- WizardsPush/Assets/Scripts/Player.cs
+++ WizardsPush/Assets/Scripts/Player.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public GameObject[] boxes;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(boxes.Length == 0)
+        {
+            boxes = GameObject.FindGameObjectsWithTag("Box");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(transform.position);
+        pull();
+    }
+
+    // Push
+    public void pull()
+    {
+        foreach(GameObject box in boxes)
+        {
+            if(box.transform.position.x == transform.position.x)
+            {
+                if(box.transform.position.y < transform.position.y)
+                {
+                    
+                }
+                else
+                {
+
+                }
+            }
+
+            else if(box.transform.position.y == transform.position.y)
+            {
+
+            }
+        }
+    }
+
+    public void push()
+    {
+
     }
 }
