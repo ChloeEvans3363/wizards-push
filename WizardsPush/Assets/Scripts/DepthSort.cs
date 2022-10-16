bcf4e8c7e44ebe861b1b4b8ea529c247b076e423
LackOfBeef <egan@frelich.us>
2022-10-16 17:59:47 -0400

    Added a placeholder box asset and added a script for depth sorting certain sprites

---
+++ WizardsPush/Assets/Scripts/DepthSort.cs
@@ -0,0 +1,22 @@
+//Author: Egan Frelich
+//Sets depth sorting order.
+//The general functionality can be moved to other scripts if we want.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DepthSort : MonoBehaviour
+{
+    private SpriteRenderer mySpriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mySpriteRenderer.sortingOrder = Mathf.RoundToInt(-transform.position.y);
+    }
+}
