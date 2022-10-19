3abd5b2477ec4648c66e4664713c42b9721728f5
LackOfBeef <egan@frelich.us>
2022-10-18 22:10:31 -0400

    Added a placeholder level select screen

---
+++ WizardsPush/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    /// <summary>
+    /// Loads the specified scene
+    /// </summary>
+    /// <param name="SceneName"></param>
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
